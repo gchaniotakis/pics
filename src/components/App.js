4e143df73b7473db033d7ad086eb0f18dae3df13
George Chaniotakis <haniotinho@gmail.com>
2019-08-09 22:13:42 +0300

    get request to Unsplash API with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 
 class App extends React.Component{
     onSearchSubmit(term){
-        console.log(term);
+       axios.get('https://api.unsplash.com/search/photos',{
+           params:{query: term},
+           headers:{
+               Authorization: 'Client-ID 09a229ef4ab40861178c50075e84090e16fa77d420941eb17cab84b35e1ee7d1'
+           }
+       }); 
     }
 
     render(){
